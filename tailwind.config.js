b5b6439103fde0a9f5daa6ee2e7fc92fcd2cfe6d
William Osborne <wosborne@enhance.group>
2024-06-17 19:44:14 +0100

    Add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ module.exports = {
       'roboto-slab': ['Roboto Slab', 'serif'],
       'roboto-mono': ['Roboto Mono', 'monospace'],
     },
+    colors: {
+      grey: '#272727',
+      white: '#ffffff',
+      blue: '#4D92D2',
+      orange: '#D2654D',
+      'pale-orange': '#FFEEEA'
+    },
     extend: {}
   },
   plugins: [],
